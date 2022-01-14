9e304943019b954a6ba9730ef10a1dc5ec345e75
callmejazz <jmeade4347@gmail.com>
2022-01-13 17:47:25 -0800

    Finished music feature and added new methods to the game

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 import numpy as np
-import sys
 import pygame
+from slowprint.slowprint import *
+
+pygame.init()
+
 
 class Music:
     def __init__(self):
@@ -13,13 +16,17 @@ class Music:
         self.victory_track = pygame.mixer.Sound("music/17_Victory (VS Trainer).mp3")
         self.current_track = self.opening_track
 
-    def  play(self, track_name):
+    def play(self, track_name):
         self.current_track.stop()
         self.current_track = self.__dict__.get(track_name)
-        self.current_track.play(loops = -1)
+        self.current_track.set_volume(0.1)
+        self.current_track.play(loops=-1)
 
     def stop(self):
         self.current_track.stop()
+
+
+music = Music()
 
 
 def delay_print(s):
@@ -27,13 +34,8 @@ def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.02)
 
-def pokemon_starter():
-            print("Hello trainer, my name is prof. Oak. You're about to go on a Pokemon Journey, but first, you need "
-                  "a pokemon. ")
-            starter = (int(input('Which pokemon will you choose? 1.Charmander, 2.Squritle, 3.Bulbasaur  ')))
-            return pokemon[starter - 1]
 
 # Making the pokemon class with stats for user pokemon
 # initialize the pokemon class and attributes
@@ -42,6 +44,7 @@ class Pokemon:
     # reference https://python-forum.io/Thread-Game-Logic-Pokemon-like-type-advantages-in-python
     def __init__(self, name, types, moves, EVs, health='==================='):
         # save variables as attributes
+
         self.name = name
         self.types = types
         self.moves = moves
@@ -49,12 +52,10 @@ class Pokemon:
         self.defense = EVs['DEFENSE']
         self.health = health
         self.bars = 20  # Amount of health bars
-        self.music = Music()
-
 
     def fight(self, rivalPokemon):
         # Allow the pokemon to fight each other
-
+        music.play("battle_track")
         # Print fight information
         print(f"\n{self.name}")
         print("TYPE/", self.types)
@@ -76,7 +77,8 @@ class Pokemon:
         # There are type advantages in Pokemon Fire is weak against water, water is weak against grass, grass is weak
         # against fire.....Fire is strong against grass, Water is strong against fire and grass is strong against water
         version = ['Fire', 'Water', 'Grass', 'Normal']
-        # website where I referenced the code below https://realpython.com/python-enumerate/#iterating-with-for-loops-in-python
+        # website where I referenced the code below
+        # https://realpython.com/python-enumerate/#iterating-with-for-loops-in-python
         for i, k in enumerate(version):
             # rivalPokemon is STRONG
             if rivalPokemon.types == version[(i + 1) % 3]:
@@ -84,24 +86,25 @@ class Pokemon:
                 rivalPokemon.defense *= 8
                 self.attack /= 2
                 self.defense /= 2
-                string_1_attack = '\nIts not very effective...'
-                string_2_attack = '\nIts super effective!'
+                string_1_attack = '\n Not very effective....'
+                string_2_attack = '\nSuper effective'
 
             # rivalPokemon is WEAK
             if self.types == version[(i + 2) % 3]:
-                self.attack *= 4
-                self.defense *= 2
+                self.attack *= 3
+                self.defense *= 3
                 rivalPokemon.attack /= 6
                 rivalPokemon.defense /= 2
-                string_1_attack = '\nIts super effective!'
-                string_2_attack = '\nIts not very effective...'
+                string_1_attack = '\nCritical Hit!'
+                string_2_attack = '\n Not very effective!'
 
         # Now for the actual fighting...
         # Continue while pokemon still have health
+        time.sleep(3)
         while (self.bars > 0) and (rivalPokemon.bars > 0):
             # Print the health of each pokemon
             print(f"\n{self.name}\t\tHLTH\t{self.health}")
-            print(f"\n{rivalPokemon.name}\t\tHLTH\t{rivalPokemon.health}\n")
+            print(f"\n{rivalPokemon.name}\t\tHLTH\t{rivalPokemon.health}")
 
             print(f"Go {self.name}!")
             for i, x in enumerate(self.moves):
@@ -126,7 +129,8 @@ class Pokemon:
 
             # Check to see if Pokemon fainted
             if rivalPokemon.bars <= 0:
-                delay_print("\n..." + rivalPokemon.name + ' fainted.')
+                delay_print("\n..." + rivalPokemon.name + ' fainted.\n')
+                self.victory()
                 break
 
             # rivalPokemon's turn
@@ -156,18 +160,52 @@ class Pokemon:
                 delay_print("\n..." + self.name + ' fainted.')
                 break
 
+    def victory(self):
+        music.play("victory_track")
         money = np.random.choice(5000)
+        delay_print("Okay! I'll make my Pokémon fight to toughen it up! Ash! Gramps! Smell you later!")
         delay_print(f"\nOpponent paid you ${money}.\n")
+        time.sleep(10)
 
-pokemon =  [
-                Pokemon('Charmander', 'Fire', ['Ember', 'Scratch', 'Tackle', 'Fire Punch'], {'ATTACK': 4, 'DEFENSE': 2}),
-                Pokemon('Squirtle', 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf'], {'ATTACK': 3, 'DEFENSE': 3}),
-                Pokemon('Bulbasaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Tackle', 'Leech Seed'],{'ATTACK': 2, 'DEFENSE': 4}),
-                Pokemon('Eevee', 'normal', ['Tackle', 'Bite', 'Take Down', 'Body Slam'], {'ATTACK': 3, 'DEFENSE': 2})
-             ]
 
+pokemon_choices = [
+
+    Pokemon('Charmander', 'Fire', ['Ember', 'Scratch', 'Tackle', 'Fire Punch'], {'ATTACK': 4, 'DEFENSE': 4}),
+    Pokemon('Squirtle', 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf'], {'ATTACK': 2, 'DEFENSE': 4}),
+    Pokemon('Bulbasaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Tackle', 'Leech Seed'], {'ATTACK': 2, 'DEFENSE': 5}),
+    Pokemon('Eevee', 'normal', ['Tackle', 'Bite', 'Take Down', 'Body Slam'], {'ATTACK': 6, 'DEFENSE': 3})
+
+]
+
+
+def pokemon_starter():
+    music.play("opening_track")
+    delay_print("Hello there! Welcome to the world of Pokémon! My name is Oak! People call me the Pokémon "
+                "Prof! This world is inhabited by creatures called Pokémon! For some people, Pokémon are "
+                "pets. Other use them for fights. \nMyself… I study Pokémon as a profession. First, "
+                "what is your name? Right! So your name is Ash! \nThis is my grandson. He's been your rival "
+                "since you were a baby. …Erm, what is his name again? That's right! I remember now! His name "
+                "is Gary!\nYour very own Pokémon legend is about to unfold! A world of dreams and "
+                "adventures with Pokémon awaits! Let's go!\n")
+    starter = (int(input('Which pokemon will you choose? 1.Charmander, 2.Squritle, 3.Bulbasaur  \n')))
+    return pokemon_choices[starter - 1]
+    time.sleep(1)
+
+
+def rival_appears():
+    music.play("rival_track")
+    delay_print("Wait Ash! Let's check out our Pokémon! Come on, I'll take you on!\n")
+    time.sleep(5)
+    return pokemon_choices[3]
+
+
+def main():
+    player = pokemon_starter()
+    rival = rival_appears()
+    player.fight(rival)
+    music.stop()
+    quit()
 
 
 if __name__ == '__main__':
-
-    pokemon_starter().fight(Pokemon[3])  # Get them to fight
+    main()
